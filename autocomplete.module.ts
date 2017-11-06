4ac7e522007dcd8bf2f1a5d410d484d9ea1bd81a
Dejan Garvanski <dgarvanski@gmail.com>
2017-11-06 17:24:42 +0200

    Remove unused import

--- autocomplete.module.ts
+++ autocomplete.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
 import { AutocompleteComponent } from "./autocomplete.component";
-import { TranslateModule } from "@ngx-translate/core";
 
 @NgModule({
   imports: [
